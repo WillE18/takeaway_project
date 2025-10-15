282c6867cce0df511e6d01b7a53a09aba141fdf4
Will Ellis <william.elli5@icloud.com>
2025-10-15 16:35:13 +0100

    add unit tests for Basket class

--- lib/basket.py
+++ lib/basket.py
@@ -1,16 +1,9 @@
 class Basket:
     def __init__(self):
-        # dishes = []
-        pass
-    def add(dish):
-        # side-effects:
-        #   adds dish to basket
-        pass
-    def remove(dish):
-        # side-effects:
-        #   removes dish from basket
-        pass
-    def view_basket(self):
-        # returns:
-        #   all dishes in the basket
-        pass
+        self._dishes = []
+    def add(self, dish):
+        self._dishes.append(dish)
+    def remove(self, dish):
+        self._dishes.remove(dish)
+    def view_all(self):
+        return self._dishes
---
+++ tests/test_basket.py
@@ -0,0 +1,21 @@
+from unittest.mock import Mock
+from lib.basket import *
+
+def test_empty_basket_initialised():
+    basket = Basket()
+    assert basket.view_all() == []
+
+def test_add_dish_to_basket():
+    dish = Mock()
+    basket = Basket()
+    basket.add(dish)
+    assert basket.view_all() == [dish]
+
+def test_add_multiple_dishes_then_remove_dish():
+    dish1 = Mock()
+    dish2 = Mock()
+    basket = Basket()
+    basket.add(dish1)
+    basket.add(dish2)
+    basket.remove(dish1)
+    assert basket.view_all() == [dish2]
