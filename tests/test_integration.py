f24edd98999ca1c78c9c29cedd30030d733b4755
Will Ellis <william.elli5@icloud.com>
2025-10-16 14:15:30 +0100

    add integration tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -5,19 +5,61 @@ from lib.receipt_generator import *
 from lib.takeaway import *
 from lib.texter import *
 
-def dont_run_yet_test_place_takeaway_order():
+dish1 = Dish("10 Chicken Wings", 
+            "10 large BBQ flavoured chicken wings",
+            ["mustard", "celery"],
+            17.90)
+dish2 = Dish("5 Chicken Wings", 
+            "5 large BBQ flavoured chicken wings",
+            ["mustard", "celery"],
+            12.90)
+dish3 = Dish("3 Chicken Wings", 
+            "3 large BBQ flavoured chicken wings",
+            ["mustard", "celery"],
+            17.90)
+
+def test_add_takeaway_dish():
     takeaway = Takeaway("Nandos", 2.99)
-    dish1 = Dish("10 Chicken Wings", 
-                "10 large BBQ flavoured chicken wings",
-                ["mustard", "celery"],
-                17.90)
-    dish2 = Dish("5 Chicken Wings", 
-                "5 large BBQ flavoured chicken wings",
-                ["mustard", "celery"],
-                12.90)
     takeaway.add_dish(dish1)
     takeaway.add_dish(dish2)
-    basket = Basket()
+    assert takeaway.menu == [dish1, dish2]
+
+def test_remove_takeaway_dish():
+    takeaway = Takeaway("Nandos", 2.99)
+    takeaway.add_dish(dish1)
+    takeaway.add_dish(dish2)
+    takeaway.remove_dish(dish1)
+    assert takeaway.menu == [dish2]
+
+
+def test_view_menu_after_adding_multiple_items():
+    takeaway = Takeaway("Nandos", 2.99)
+    takeaway.add_dish(dish1)
+    takeaway.add_dish(dish2)
+    assert takeaway.menu == [dish1, dish2]
+
+def test_place_takeaway_order():
+    takeaway = Takeaway("Nandos", 2.99)
+    takeaway.add_dish(dish1)
+    takeaway.add_dish(dish2)
+    basket = Basket(takeaway)
     basket.add(dish1)
     basket.add(dish2)
-    takeaway.place_order(basket)
+    order = takeaway.place_order(basket)
+    assert takeaway.order_history == [order]
+
+def test_order_history_after_placing_multiple_orders():
+    takeaway = Takeaway("Nandos", 2.99)
+    takeaway.add_dish(dish1)
+    takeaway.add_dish(dish2)
+    takeaway.add_dish(dish3)
+    basket1 = Basket(takeaway)
+    basket1.add(dish1)
+    basket1.add(dish2)
+    order1 = takeaway.place_order(basket1)
+    basket2 = Basket(takeaway)
+    basket2.add(dish2)
+    basket2.add(dish3)
+    order2 = takeaway.place_order(basket2)
+    assert takeaway.order_history == [order1, order2]
+
