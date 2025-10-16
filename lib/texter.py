153b68e88f187f3cb613b35966fbc0325477cef2
Will Ellis <william.elli5@icloud.com>
2025-10-16 13:32:05 +0100

    update texter unit test

--- lib/texter.py
+++ lib/texter.py
@@ -1,9 +1,6 @@
-
-
 class Texter:
-    def __init__(self, order, text_sender):
+    def __init__(self, order):
         self.order = order
-        self.text_sender = text_sender
     def send(self):
         delivery_time = self.order.get_predicted_delivery_time().time().strftime("%H:%M")
         message = f"Thank you! Your order was placed and will be delivered before {delivery_time}"
--- tests/test_texter.py
+++ tests/test_texter.py
@@ -6,8 +6,7 @@ def test_texter_order_initialised():
     takeaway = Mock()
     takeaway.place_order.return_value = Mock()
     order = takeaway.place_order()
-    text_sender = Mock()
-    texter = Texter(order, text_sender)
+    texter = Texter(order)
     assert texter.order == order
 
 def test_texter_send_text():
@@ -16,7 +15,5 @@ def test_texter_send_text():
     order = takeaway.place_order()
     order.order_time = datetime(2025, 7, 21, 17, 30)
     order.get_predicted_delivery_time.return_value = datetime(2025, 7, 21, 18, 00)
-    text_sender = Mock()
-    text_sender.send.return_value = "Thank you! Your order was placed and will be delivered before 18:00"
-    texter = Texter(order, text_sender)
+    texter = Texter(order)
     assert texter.send() == "Thank you! Your order was placed and will be delivered before 18:00"
